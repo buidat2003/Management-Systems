30b35e1b7b87e91b69d682a08b8f2312051459aa
Nguyễn Quang Trung <trungnguye.081203@gmail.com>
2024-11-04 15:10:06 +0700

    Project Base Create Template By Bùi Thông

---
+++ Mock/src/main/java/org/example/mock/Controller/HomeController.java
@@ -0,0 +1,12 @@
+package org.example.mock.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String home() {
+
+        return "home"; // Đây là tên của file template (home.html) trong thư mục templates
+    }
+}
