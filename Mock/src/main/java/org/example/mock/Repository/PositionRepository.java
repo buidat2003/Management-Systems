abb1c93b3f876e098752ef8b445e46b631a22483
dat <datbthe173467@fpt.edu.vn>
2024-11-08 10:19:47 +0700

    ViewJobList

--- Mock/src/main/java/org/example/mock/Repository/PositionRepository.java
+++ Mock/src/main/java/org/example/mock/Repository/PositionRepository.java
@@ -4,6 +4,8 @@ import org.example.mock.Model.PositionAll;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PositionRepository extends JpaRepository<PositionAll, Long> {
 }
--- Mock/src/main/java/org/example/mock/Service/DepartmentService.java
+++ Mock/src/main/java/org/example/mock/Service/DepartmentService.java
@@ -1,4 +1,5 @@
 package org.example.mock.Service;
 
 public class DepartmentService {
+
 }
